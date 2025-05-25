12f8c40acc71fc3364bd26fef92148cec2bd04f3
Eric Wen <ew21hi@gmail.com>
2025-05-24 22:43:12 -0400

    novel parser

--- js/novelParser.js
+++ js/novelParser.js
@@ -1,19 +1,29 @@
-// @ts-nocheck
+
 
 class BaseParser {
+    novel = 0;
+    chapter = 1;
     constructor(url,novel,chapter) {
         this.url = url;
+
+        /** @type {string[]} */
         this.tokens = new URL(url).pathname.split("/").filter(Boolean);
         this.novel=novel;
         this.chapter=chapter;
     }
 
     getNovel() {
-        return null;
+        if (this.tokens.size()<=this.novel) {
+            return "";
+        }
+        return this.tokens[this.novel];
     }
 
     getChapter() {
-        return null;
+        if (this.tokens.size()<=this.chapter) {
+            return this.chapter;
+        }
+        return this.tokens[this.chapter];
     }
 
     printTokens() {
