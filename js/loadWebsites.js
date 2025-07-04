0cb5fa8dfd97b89040dfb9bfb79170244e8a5276
Eric Wen <ew21hi@gmail.com>
2025-07-04 17:36:44 -0400

    displays novels

--- js/loadWebsites.js
+++ js/loadWebsites.js
@@ -2,17 +2,20 @@
 
 let trackedWebsites = [];
 let websiteTemplate;
-
+let main;
 let novelTemplate;
 
 InitializePage()
+let lastWebsite = new Website();
+
 
 async function InitializePage() {
     websiteTemplate = document.getElementById("website-row-template");
     novelTemplate = document.getElementById("novel-row-template");
     const result = await chrome.storage.local.get(["trackedWebsites"]);
     trackedWebsites = new Set(result.trackedWebsites || []);
-    LoadWebsites();
+    main = document.getElementById("info-rows-template");
+    await LoadWebsites();
 }
 
 
@@ -20,29 +23,59 @@ async function InitializePage() {
 async function LoadWebsites() {
     // add the main div
     
-    trackedWebsites.forEach(site => LoadSingleWebsite(site));
+    for (const site of trackedWebsites) {
+        await LoadSingleWebsite(site);
+    }
     
 }
 
 async function LoadSingleWebsite(site) {
-    let parent = websiteTemplate.cloneNode(true);
-	parent.removeAttribute('id');
+    
+    let parent = websiteTemplate.content.cloneNode(true);
+    console.log(site);
+    let websiteName = parent.querySelector('.website');
+    websiteName.textContent=site;
+    container = parent.querySelector('.novels-inside-websites');
+    container.removeAttribute('id');
+	
 
-    await LoadNovels(parent,site);
-
+    await LoadNovels(container,site);
+    main.append(parent);
+    
+   
     // append the div to the main div
 
 }
 
 async function LoadNovels(parent,site) {
-    let node = novelTemplate.cloneNode(true);
-    node.removeAttribute('id');
-    // create novel divs
-    LoadSingleNovel(site);
+    
+    // create novel divs    
+
+    const result = await chrome.storage.local.get([site]);
+    let website = Website.fromJSON(result[site]);
+
+    
+    for (const novel of website.novels) {
+        const node = await LoadSingleNovel(novel);
+        parent.append(node);
+    }
+    
     // append novel divs to the website div
-    let main = document.getElementById("info-rows")
+    
 }
 
-async function LoadSingleNovel(site) {
+async function LoadSingleNovel(novel) {
+    console.log(novel.name);
+    let node = novelTemplate.content.cloneNode(true);
     
+    let name = node.querySelector('.novel-name');
+    
+    name.textContent = novel.name;
+
+    let link = node.querySelector('.novel-link');
+    link.textContent = novel.lastChapter;
+    
+    return node;
+    
+
 }
